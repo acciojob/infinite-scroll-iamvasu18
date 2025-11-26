2ad34552ece9181cc17d4d43f6d339411c627b1b
Dharavath Vasu Naik <168115062+iamvasu18@users.noreply.github.com>
2025-11-26 22:41:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 //your code here!
+const ol=document.getElementById("infi-list");
+let count=1;
+function additems(num){
+	for(int i=0;i<num;i++){
+		const li=document.createElement("li");
+		ol.textContent=`Item ${count}`;
+		ol.appendChild(li);
+	}
+}
 
+additems(10);
+list.addEventListener("scroll", () => {
+  if (list.scrollTop + list.clientHeight >= list.scrollHeight) {
+    addItems(2);
+  }
+});
